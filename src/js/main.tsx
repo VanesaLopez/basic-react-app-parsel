c90a8ffc36814ab3b311295563cc0a98caf2296e
VanesaLopez <avanesalopez@hotmail.com>
2019-06-26 23:55:29 +0200

    Added dependencies

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import '@babel/polyfill';
+import '../css/main.scss';
 import { HelloComponent } from "./components/helloComponent";
 
 render(<HelloComponent />, document.getElementById('mainApp'))
